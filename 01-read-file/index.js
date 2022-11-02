e27b576297a30433a03811e26ad346e40d85729b
Maria Koroleva <mkoroleva5@gmail.com>
2022-11-02 16:57:02 +0300

    feat: create script for task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readableStream.on('data', data => stdout.write(data));
+readableStream.on('error', error => console.log('Error', error.message));
